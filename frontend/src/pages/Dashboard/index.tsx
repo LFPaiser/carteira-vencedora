be7ba24525d6da05f553cbe0f5349f7acd38a773
Luis Felipe Damiani Paiser <felipe.paiser@gmail.com>
2020-10-20 17:30:19 -0300

    Added code format config and setup default page with an axios instance

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+  return <h1>Hello B3!</h1>
+}
+
+export default Dashboard
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+    </Switch>
+  )
+}
