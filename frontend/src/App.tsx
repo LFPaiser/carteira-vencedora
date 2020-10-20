8ac7d3f1e8c1cceb910f424ff0abc2027ad20e51
Luis Felipe Damiani Paiser <felipe.paiser@gmail.com>
2020-10-20 19:06:06 -0300

    Preparaçao para uma pagina simples com uma instancia do axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import GlobalStyle from './styles/global'
+// import GlobalStyle from './styles/global'
 import Routes from './routes'
 
 const App: React.FC = () => {
@@ -9,7 +9,6 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
-      <GlobalStyle />
     </>
   )
 }
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Dashboard from './pages/Dashboard'
+import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => {
   return (
@@ -10,3 +10,5 @@ const Routes: React.FC = () => {
     </Switch>
   )
 }
+
+export default Routes
